ad7fcc6bfbe60a35de715f3f7143c36ecad634ae
6y6a <gubinalexeymain@gmail.com>
2016-09-06 19:19:39 +0300

    Добавление функциональности всплывающего окна

--- src/blocks/room-slider/room-slider.js
+++ src/blocks/room-slider/room-slider.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
   $owl = $('#room-slider');
   $left = $owl.parent().find('.room-slider__left');
   $right = $owl.parent().find('.room-slider__right');
+  $popup = $owl.parent().find('.room-slider__popup');
 
 
   $owl.owlCarousel({
@@ -10,12 +11,32 @@ $(document).ready(function () {
     dots: false
   });
 
+  $owl.magnificPopup({
+    //disableOn: function() {
+      //return false;
+    //},
+    type: 'image',
+    delegate: 'img',
+    gallery:{
+      enabled:true,
+      navigateByImgClick: true
+    }
+  });
+
+  $owl.on('click', function (e) {
+    e.preventDefault();
+  });
+
   $left.on('click', function(){
     $owl.trigger('prev.owl.carousel');
   });
 
   $right.on('click', function(){
     $owl.trigger('next.owl.carousel');
+  });
+
+  $popup.on('click', function () {
+    $owl.magnificPopup('open');
   });
 
 });
